cdcab69c46aead2aa062a6f461346bc31e8924b5
derrickcheng <derrick@eecs.berkeley.edu>
2014-02-20 10:49:51 -0800

    allowing testCumSumG to run cumsumg multiple times

--- tests/testCumSumG.scala
+++ tests/testCumSumG.scala
@@ -3,6 +3,7 @@ import BIDMach.models.RandomForest
 import BIDMach.models.EntropyEval
 
 // :load tests/testCumSumG.scala
+val numTimesToRun = 10
 val matName = "/home/derrick/code/NewRandomForest/BIDMach/tests/leftImpurityCumSumG.mat"
 // saveAs(matName, FMat(pctsts), "pctsts", IMat(jc), "jc", FMat(leftAccumPctsts), "leftAccumPctsts")
 val pct : FMat = load(matName,"pctsts")
@@ -15,6 +16,11 @@ println("Running CumSumG:")
 val result = cumsumg(pctsts, jc, null);
 // println("Recorded Output")
 // println(leftAccumPctsts)
-println("CumSumG Output:")
-println(result)
-println("If you run this script multiple times you will notice that the Output of CumSumG is different each time")
+var i = 0
+while (i < numTimesToRun) {
+	println("Iteration #" + i)
+	println("CumSumG Output:")
+	println(result)
+	i = i + 1
+}
+println("CumSumG is run multiple times. The Output of CumSumG is not always the same.)
